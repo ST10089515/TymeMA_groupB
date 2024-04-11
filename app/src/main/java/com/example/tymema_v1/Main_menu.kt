73924398cdefa262749350418cc4648ab4d52b64
Motjoka Fanana <st10089515@vcconnect.edu.za>
2024-04-11 13:48:13 +0200

    main

---
+++ app/src/main/java/com/example/tymema_v1/Main_menu.kt
@@ -0,0 +1,11 @@
+package com.example.tymema_v1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main_menu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_login2)
+    }
+}
